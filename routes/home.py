11f2418c9ad8a90060935ee33425a343e11de747
Joel _ <joel8023@outlook.com>
2025-08-02 08:40:48 +0200

    ping

--- routes/home.py
+++ routes/home.py
@@ -2,12 +2,12 @@
 FilePath: routes/home.py
 Author: Joel
 Date: 2025-07-31 22:53:32
-LastEditTime: 2025-08-01 18:39:34
+LastEditTime: 2025-08-02 08:39:38
 Description: 首页 & 统计
 """
 from flask import Blueprint, render_template, request,send_file
 from models.models import VisitStats
-from datetime import datetime
+from datetime import datetime,timedelta
 from utils.get_client_ip import get_client_ip
 from models import db
 import os
@@ -35,8 +35,33 @@ def track_home_visit():
 
 @home_bp.route('/')
 def index():
+    update_last_visit()
     total_visits = db.session.query(db.func.sum(VisitStats.visit_count)).scalar() or 0
     return render_template("index.html", total_visits=total_visits)
+
+#ping
+path = os.path.join('static', 'last_visit.txt')
+# 读取上次访问时间
+def read_last_visit():
+    if not os.path.exists(path):
+        return None
+    with open(path, 'r') as f:
+        try:
+            return datetime.fromisoformat(f.read().strip())
+        except:
+            return None
+#更新上次访问时间
+def update_last_visit():
+    with open(path, 'w') as f:
+        f.write(datetime.utcnow().isoformat())
+@home_bp.route('/ping')
+def ping():
+    last_visit = read_last_visit()
+    now = datetime.utcnow()
+    if last_visit is None or (now - last_visit) > timedelta(minutes=13.5):
+        update_last_visit()
+        return 'pong', 200
+
 #检查用
 @home_bp.route('/download_db')
 def download_db():
